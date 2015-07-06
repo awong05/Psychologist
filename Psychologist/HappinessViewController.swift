34724f3812b795ab203009dd507f4b9bcde234bc
Andy Wong <awong@propelmarketing.com>
2015-07-06 18:21:33 -0400

    Implementing UINavigationController wrapping.

--- Psychologist/HappinessViewController.swift
+++ Psychologist/HappinessViewController.swift
@@ -37,7 +37,8 @@ class HappinessViewController: UIViewController, FaceViewDataSource
     
     private func updateUI()
     {
-        faceView.setNeedsDisplay()
+        faceView?.setNeedsDisplay()
+        title = "\(happiness)"
     }
     
     func smilinessForFaceView(sender: FaceView) -> Double? {
